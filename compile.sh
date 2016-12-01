0575dbcf6393b170db8d9807c14d8d2074f0fce0
tetrovolt <raymond@sutrisno.me>
2016-12-01 17:22:02 -0600

    first commit

---
+++ compile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#g++ -W -Wall `sdl2-config --cflags --libs` -lSDL_ttf -O0 -pedantic -std=c++11 -o ./out/program ./src/*.cpp
+
+g++ -W -Wall `sdl2-config --cflags --libs` -O0 -pedantic -std=c++11 -o ./out/program ./src/*.cpp
+
+
+
