f9b7b6e7698c109a360a8295f5580e295033d16f
xylafur <jkesleyrichardson@gmail.com>
2016-12-04 01:17:14 -0600

    insert method, need obj class

--- RKGC.cpp
+++ RKGC.cpp
@@ -3,7 +3,28 @@ class RKGC{
   public:
 
   private:
-    long unsigned int * OBJ = (long unsigned int*)malloc(sizeof(long unsigned int ) * 10) ;
+    long unsigned int * OBJ;
+
+  RKGC(){
+    OBJ = (long unsigned int*)malloc(sizeof(long unsigned int ) * 10) ;
+  }
+
+  int insert(GameObject inObj){
+    if(full())return 1;
+
+    void *temp = OBJ;
+    int count = 0;
+
+    while(*temp != NULL){
+      temp++;
+      count++;
+    }
+
+    OBJ[count] = inObj;
+
+
+  }
+
 };
 
 
